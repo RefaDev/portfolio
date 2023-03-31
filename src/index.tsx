c9c51fda72eedd68e549d30957eed909516ced34
Refat Abdukarimov <usergit@mail.ru>
2023-03-31 16:41:39 +0300

    123123

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ScrollToTop from 'utils/scrollToTop'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <BrowserRouter basename='/portfolio'>
+  <HashRouter basename='/portfolio'>
     <ScrollToTop />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
