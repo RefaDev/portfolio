1fcbd21c998c4765051825237a301f68ae700fbd
Refat Abdukarimov <usergit@mail.ru>
2023-03-31 16:05:47 +0300

    added strange functions from stuckoverflow

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
 
           <h2 className='text' translate='no'>
             Let me tell{' '}
-            <NavLink to='/portfolio/about'>
+            <NavLink to='/about'>
               <span>about myself.</span>
             </NavLink>
           </h2>
